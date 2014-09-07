0109f3b2eed958eede583aaeb98367e5f27f8f77
Jaskirat <j.khangoora@gmail.com>
2014-09-06 23:26:23 -0400

    Initial Commit

---
+++ Pirate Text Game/JSKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JSKAppDelegate.h
+//  Pirate Text Game
+//
+//  Created by Jaskirat Khangoora on 9/6/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Text Game/JSKViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSKViewController.h
+//  Pirate Text Game
+//
+//  Created by Jaskirat Khangoora on 9/6/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSKViewController : UIViewController
+
+@end
